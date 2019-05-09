a8c42d06d431c56fbf0e526e71ccd223cc51be0f
diiorio7 <32276581+diiorio7@users.noreply.github.com>
2019-05-09 18:00:16 -0300

    criando um arquivo js para a sidenavbar

---
+++ view/js/js-navbar.js
@@ -0,0 +1,11 @@
+var nav = document.getElementById("sidenavbar");
+nav.addEventListener("mouseenter", function(){
+
+    nav.style.width = "200px";
+
+});
+
+nav.addEventListener("mouseleave", function(){
+
+    nav.style.width = "0px";
+});
