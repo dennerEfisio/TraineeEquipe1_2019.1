8478d48223eda80dac3be3a1f7bcf7cca8a52f50
diiorio7 <32276581+diiorio7@users.noreply.github.com>
2019-06-15 17:20:54 -0300

    arrumando os espacamentos

--- controllers/cargoController.php
+++ controllers/cargoController.php
@@ -3,23 +3,14 @@
 class CargoController
 {
 
-
      public function index()
     {
-
-
         $cargos = App::get('database')->selectAll('cargo');
         return view('index', compact('cargos'));
     }
 
     public function store(){
-
-
         $cargos = App::get('database')->store('cargo');
         return view('create', compact('user'));
     }
-
-    
-
-    
 }
